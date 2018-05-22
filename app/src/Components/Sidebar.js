c0051dfd00f461acb9e171557e58f3ac9fbf4f0c
dhansenaz <dhansen@allthingsdot.com>
2018-05-21 17:08:21 -0700

    sidebar

--- app/src/Components/Sidebar.js
+++ app/src/Components/Sidebar.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from 'axios'
 import "./sidebar.css";
 import { Link } from "react-router-dom";
-import logo from "../logo.png";
+// import logo from "../logo.png";
 
-export default function Sidebar() {
+class Sidebar extends Component {
+constructor(){
+    super()
+
+    this.state={
+        user: []
+    }
+}
+
+// componentDidMount(){
+
+//     axios.get(`/api/  /${this.state.user.id}`).then((r) => {
+//     this.setState({ user: r.data})
+        
+//     })
+//   }
+    render(){
   return (
     <div className="d-side-bar">
       <aside>Sidebar</aside>
     </div>
   );
 }
+}
+export default Sidebar
