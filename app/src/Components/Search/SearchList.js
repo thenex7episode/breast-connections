50b0c230e6d98361b01832d459a6d02f9bf5f55e
Benedikt Mix <mixbened@gmail.com>
2018-05-24 10:38:43 -0700

    merged stuff

--- app/src/Components/Search/SearchList.js
+++ app/src/Components/Search/SearchList.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class SearchList extends Component {
     render() {
         return (
-            <div></div>
+            <div>
+                
+            </div>
         );
     }
 }
