edbabe7bf6c7f8e69eaa4ea2b6dcc13a33529e17
Benedikt Mix <mixbened@gmail.com>
2018-05-31 11:16:41 -0700

    googlesearch plus experiences

--- app/src/Components/Search/Search.js
+++ app/src/Components/Search/Search.js
@@ -53,25 +53,27 @@ export default class Search extends Component {
         // console.log('--render Search!')
         const { selectedTags } = this.state;
         return (
-            <div className='searchContainer' style={{padding: '5em', textAlign: 'center'}}>
-                <h1>Search for Doctors and see rankings by BC Users</h1>
-                <InputGroup style={{margin: '0 auto'}}compact>
-                    <Select defaultValue="doctor" style={{ width: 120 }} onChange={(e) => this.setState({selectedType: e})}>
-                        <Option value="doctor">Doctors</Option>
-                        <Option value="hospital">Hospitals</Option>
-                        <Option value="pharmacy">Pharmacies</Option>
-                        <Option value="insurance_agency">Insurance Agencies</Option>
-                    </Select>
-                    <Input style={{width: '50%'}} value={this.state.selectedLocation} onChange={e => {
-                        this.setState({selectedLocation: e.target.value})
-                        this.isTyping()
-                    }}/>
-                    <Button>Search</Button>
-                </InputGroup>
-                <div style={{display: 'flex', justifyContent: 'space-between', width: '50%', margin: '0 auto', padding: '1em', alignItems: 'center'}}>
-                    <div>Search Radius</div>
-                    <Slider style={{margin: '1em auto', width: '50%', display: 'inline-block'}} tipFormatter={v => `${v} km`} onChange={e => this.setState({radius: e})} min={10} max={50} defaultValue={30} disabled={false} />
-                    <div>{this.state.radius}</div>
+            <div>
+                <div className='searchContainer'>
+                    <h1>Search for Doctors and see rankings by BC Users</h1>
+                    <InputGroup style={{margin: '0 auto'}}compact>
+                        <Select defaultValue="doctor" style={{ width: 120 }} onChange={(e) => this.setState({selectedType: e})}>
+                            <Option value="doctor">Doctors</Option>
+                            <Option value="hospital">Hospitals</Option>
+                            <Option value="pharmacy">Pharmacies</Option>
+                            <Option value="insurance_agency">Insurance Agencies</Option>
+                        </Select>
+                        <Input style={{width: '50%'}} value={this.state.selectedLocation} onChange={e => {
+                            this.setState({selectedLocation: e.target.value})
+                            this.isTyping()
+                        }}/>
+                        <Button onClick={() => this.getGoogleResults()}>Get Results</Button>
+                    </InputGroup>
+                    <div style={{display: 'flex', justifyContent: 'space-between', width: '50%', margin: '0 auto', padding: '1em', alignItems: 'center'}}>
+                        <div>Search Radius</div>
+                        <Slider style={{margin: '1em auto', width: '50%', display: 'inline-block'}} tipFormatter={v => `${v} km`} onChange={e => this.setState({radius: e})} min={10} max={50} defaultValue={30} disabled={false} />
+                        <div>{this.state.radius}</div>
+                    </div>
                 </div>
                 <SearchList results={this.state.results}/>
             </div>
