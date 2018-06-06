644a324ddd06a5c9cb8f24863fa1d5ee5d760150
dhansenaz <dhansen@allthingsdot.com>
2018-06-06 16:25:15 -0700

    chatroom changes

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import App from './App'
-import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, {shallow, render, mount} from 'enzyme'
-import { createSerializer}  from "enzyme-to-json"
-import sinon from 'sinon'
+
 
 describe('<App />', () => {
     it('renders 1 <App /> component', () =>{
--- app/src/Components/Chat/Chatrooms.js
+++ app/src/Components/Chat/Chatrooms.js
@@ -71,13 +71,9 @@ export default class Chatroom extends Component {
     }
 
     render() {
-<<<<<<< HEAD
-        const userList = this.state.users.map(el => <li onClick={() => this.setState({receiver: el.username})}>{el.username}</li>);
-        // const chatList = this.state.chats.ma
-=======
+        
         const userList = this.state.users.map(el => <li onClick={() => this.setState({receiver: el.username, receiver_id: el.user_id})}>{el.username}</li>);
         const chatList = this.state.chats.ma
->>>>>>> e7a747cdda6734ca6aa412bbaa2545d6e65fe6c4
         return (
             <div style={{marginTop: '5em'}}>
                 <h2>Start a new chat</h2>
