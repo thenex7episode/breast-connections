332a296bdcfe7d925de798d2cf801612d49d855a
dhansenaz <dhansen@allthingsdot.com>
2018-06-07 16:49:07 -0700

    code formatting

--- app/src/Components/Forums.js
+++ app/src/Components/Forums.js
@@ -1,95 +1,141 @@
-import React, {Component} from 'react'
-import './forums.css'
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import "./forums.css";
+import { Link } from "react-router-dom";
+import Banner from "../Components/Banner/Banner";
 
+class Forums extends Component {
+  constructor() {
+    super();
 
-class Forums extends Component{
-    constructor(){
-        super()
+    this.state = {
+      categories: []
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Banner text1={"Connect."} text2={"Inspire."} text3={"Thrive."} />
 
-        this.state = {
+        <div className="section-a-wrapper">
+          <div className="section-a">
+            <h1>
+              <strong>FAMILY</strong>
+            </h1>
 
-            categories: []
-        }
-    }
-    render(){
-
-        return(
-            <div>
-                <div className='forum-header'>
-                        <h1 className='d-header-connect'>Connect.</h1>
-                        <h1 className ='d-header-inspire'>Inspire.</h1>
-                        <h1 className='d-header-love'>Thrive.</h1>
-                        
-                        </div>
-                
-                <div className='section-a-wrapper'>
-                    <div className='section-a'>
-                        <h1><strong>FAMILY</strong></h1>
-                           
-                            <div className='family-content'>
-                        <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        <Link to='/dashboard/family'><button>see posts</button></Link>
-                        
-                    </div>
-                    </div>
-                    
-                    <div className='section-b'>
-                        <h1><strong>RESOURCES</strong></h1>
-                        <div className='family-content'>
-                        <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        <Link to='/dashboard/resources'><button>see posts</button></Link>
-                        
-                    </div>
-                        
-                           
-                            
-                    </div>
-                    <div className='section-c'>
-                        <h1><strong>NUTRITION</strong></h1>
-                        <div className='family-content'>
-                        <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        <Link to='/dashboard/nutrition'><button>see posts</button></Link>
-                        
-                    </div>
-                        
-                          
-                            
-                    </div>
-                    <div className='section-d'>
-                        <h1><strong>MOTIVATION</strong></h1>
-                        <div className='family-content'>
-                        <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        <Link to='/dashboard/motivation'><button>see posts</button></Link>
-                        
-                    </div>
-                        
-                    
-                            
-                    </div>
-                    <div className='section-e'>
-                        <h1><strong>HEALTH</strong></h1>
-                        <div className='family-content'>
-                        <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        <Link to='/dashboard/health'><button>see posts</button></Link>
-                        
-                    </div>
-                        
-                            
-                    </div>
-                    <div className='section-f'>
-                        <h1><strong>FINANCIAL</strong></h1>
-                        <div className='family-content'>
-                        <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        <Link to='/dashboard/financial'><button>see posts</button></Link>
-                        
-                    </div>
-                            
-                            
-                    </div>
-                </div>
+            <div className="family-content">
+              <p>
+                {" "}
+                It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </p>
+              <Link to="/dashboard/family">
+                <button>see posts</button>
+              </Link>
             </div>
-        )
-    }
+          </div>
+
+          <div className="section-b">
+            <h1>
+              <strong>RESOURCES</strong>
+            </h1>
+            <div className="family-content">
+              <p>
+                {" "}
+                It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </p>
+              <Link to="/dashboard/resources">
+                <button>see posts</button>
+              </Link>
+            </div>
+          </div>
+          <div className="section-c">
+            <h1>
+              <strong>NUTRITION</strong>
+            </h1>
+            <div className="family-content">
+              <p>
+                {" "}
+                It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </p>
+              <Link to="/dashboard/nutrition">
+                <button>see posts</button>
+              </Link>
+            </div>
+          </div>
+          <div className="section-d">
+            <h1>
+              <strong>MOTIVATION</strong>
+            </h1>
+            <div className="family-content">
+              <p>
+                {" "}
+                It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </p>
+              <Link to="/dashboard/motivation">
+                <button>see posts</button>
+              </Link>
+            </div>
+          </div>
+          <div className="section-e">
+            <h1>
+              <strong>HEALTH</strong>
+            </h1>
+            <div className="family-content">
+              <p>
+                {" "}
+                It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </p>
+              <Link to="/dashboard/health">
+                <button>see posts</button>
+              </Link>
+            </div>
+          </div>
+          <div className="section-f">
+            <h1>
+              <strong>FINANCIAL</strong>
+            </h1>
+            <div className="family-content">
+              <p>
+                {" "}
+                It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </p>
+              <Link to="/dashboard/financial">
+                <button>see posts</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-export default Forums
+export default Forums;
--- app/src/Components/UserHome/UserHome.js
+++ app/src/Components/UserHome/UserHome.js
@@ -1,135 +1,176 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import './userhome.css'
-import Post from '../Post/Post'
-import Sidebar from '../Sidebar/Sidebar'
-import { Menu, Dropdown, Button } from 'antd';
+import React, { Component } from "react";
+import axios from "axios";
+import "./userhome.css";
+import Post from "../Post/Post";
+import Sidebar from "../Sidebar/Sidebar";
+import { Menu, Dropdown, Button } from "antd";
+import Banner from "../../Components/Banner/Banner";
 
+class UserHome extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      posts: [],
+      loggedUser: "",
+      filteredPosts: [],
+      category: ""
+    };
+    this.deletePost = this.deletePost.bind(this);
+  }
 
-class UserHome extends Component{
-    constructor(){
-        super()
+  componentDidMount() {
+    axios.get("/api/posts/").then(response => {
+      console.log("all posts-----------------------------", response);
+      this.setState({ posts: response.data, filteredPosts: response.data });
+    });
 
-        this.state = {
-            posts: [],
-            loggedUser: '',
-            filteredPosts: [],
-            category:''
-        }
-        this.deletePost = this.deletePost.bind(this)
-    }
-    
-    componentDidMount(){
-        axios.get('/api/posts/').then(response => {
-            console.log('all posts-----------------------------',response)
-            this.setState({posts: response.data, filteredPosts: response.data});
-        })
+    axios.get(`/api/check-session/`).then(data => {
+      console.log("session res", data);
+      this.setState({ loggedUser: data.data.username });
+    });
+  }
 
-        axios.get(`/api/check-session/`).then( data => {
-            console.log('session res', data)
-            this.setState({loggedUser: data.data.username})
-        })
-    }
+  deletePost(id) {
+    axios.delete(`/api/deletepost/${id}`).then(data => {
+      console.log("data.data in delete in profile:", data.data);
+      this.setState({ posts: data.data.data });
+    });
+  }
+  selectCategory(category) {
+    console.log(this.state.posts);
+    console.log("category-------------", category);
+    const filteredPosts = this.state.posts.filter(el => {
+      return el.category === category;
+    });
+    console.log("filtered posts----------", filteredPosts);
+    this.setState({ filteredPosts: filteredPosts });
+    console.log("filtered posts---------------------", this.state);
+  }
 
-
-    deletePost(id){
-        axios.delete(`/api/deletepost/${id}`).then(data => {
-            console.log('data.data in delete in profile:', data.data)
-            this.setState({posts: data.data.data})
-        })
-    }
-    selectCategory(category){
-        console.log(this.state.posts)
-        console.log('category-------------',category)
-        const filteredPosts = this.state.posts.filter((el) => {
-           
-           return el.category === category
-           
-       });
-       console.log('filtered posts----------',filteredPosts)
-       this.setState({filteredPosts:filteredPosts})
-               console.log('filtered posts---------------------',this.state)
-        
-       
-}
-    
-
-    render(){
-        const allPosts = this.state.filteredPosts.map((el,i) => {
-            return (
-              <div className='d-posts-wrapper'>
-              <div className='d-posts-content'>
-            <Post loggedUser={this.state.loggedUser} key={i} post_id={el.post_id} title={el.title} body={el.body} user_id={el.user_id} date={el.date} tracker={el.tracker} deletePostFn={this.deletePost}/>
-                </div>
-                </div>
-            )
-        })
-        const menu = (
-            <Menu>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer"  onClick={ () => {
-                    this.setState({category: 'motivation' })
-                    this.selectCategory('motivation')
-                    }}>Motivation</a>
-              </Menu.Item>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" onClick={ () => {
-                    this.setState({category:'resources'})
-                    this.selectCategory('resources')
-                }}>Resources</a>
-              </Menu.Item>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" onClick={ () => {
-                    this.setState({category: 'family'})
-                    this.selectCategory('family')
-                }}>Family</a>
-              </Menu.Item>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" onClick={ () => {
-                    this.setState({category: 'nutrition'})
-                    this.selectCategory('nutrition')
-                }}>Nutrition</a>
-              </Menu.Item>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" onClick={ () => {
-                    this.setState({category: 'health'})
-                    this.selectCategory('health')
-                }}>Health</a>
-              </Menu.Item>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" onClick={ () => {
-                    this.setState({category: 'financial'})
-                    this.selectCategory('financial')
-                }}>Financial</a>
-              </Menu.Item>
-            </Menu>
-          );
-        return <div>
-            <div className="d-userhome-header">
-
-                    <h1 className='d-connect'>Connect.</h1>
-                    <h1 className='d-inspire'>Inspire.</h1>
-                    <h1 className='d-thrive'>Thrive.</h1>
-                
-            </div>
-            <div className='category-dropdown'>
-            <Dropdown overlay={menu} placement="bottomCenter">
-                <Button>Categories</Button>
-            </Dropdown>
-                <div className='category-current'>
-                    <button className='category-button'>Current Category is {this.state.category}</button>
-                </div>
-                <Button onClick={ () => this.setState({
-                    filteredPosts: this.state.posts
-                })}
-                    
-                >Reset</Button>
-                </div>
-            
-            {allPosts}
-            
+  render() {
+    const allPosts = this.state.filteredPosts.map((el, i) => {
+      return (
+        <div className="d-posts-wrapper">
+          <div className="d-posts-content">
+            <Post
+              loggedUser={this.state.loggedUser}
+              key={i}
+              post_id={el.post_id}
+              title={el.title}
+              body={el.body}
+              user_id={el.user_id}
+              date={el.date}
+              tracker={el.tracker}
+              deletePostFn={this.deletePost}
+            />
+          </div>
         </div>
-     }
+      );
+    });
+    const menu = (
+      <Menu>
+        <Menu.Item>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => {
+              this.setState({ category: "motivation" });
+              this.selectCategory("motivation");
+            }}
+          >
+            Motivation
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => {
+              this.setState({ category: "resources" });
+              this.selectCategory("resources");
+            }}
+          >
+            Resources
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => {
+              this.setState({ category: "family" });
+              this.selectCategory("family");
+            }}
+          >
+            Family
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => {
+              this.setState({ category: "nutrition" });
+              this.selectCategory("nutrition");
+            }}
+          >
+            Nutrition
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => {
+              this.setState({ category: "health" });
+              this.selectCategory("health");
+            }}
+          >
+            Health
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => {
+              this.setState({ category: "financial" });
+              this.selectCategory("financial");
+            }}
+          >
+            Financial
+          </a>
+        </Menu.Item>
+      </Menu>
+    );
+    return (
+      <div>
+        <Banner text1={"Connect."} text2={"Inspire."} text3={"Thrive."} />
+
+        <div className="category-dropdown">
+          <Dropdown overlay={menu} placement="bottomCenter">
+            <Button>Categories</Button>
+          </Dropdown>
+          <div className="category-current">
+            <button className="category-button">
+              Current Category is {this.state.category}
+            </button>
+          </div>
+          <Button
+            onClick={() =>
+              this.setState({
+                filteredPosts: this.state.posts
+              })
+            }
+          >
+            Reset
+          </Button>
+        </div>
+
+        {allPosts}
+      </div>
+    );
+  }
 }
 export default UserHome;
